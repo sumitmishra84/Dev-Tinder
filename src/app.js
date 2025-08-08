29d4aefe0e7f94d4766eca0353178fd1ffa3624e
Sumit Mishra <sm0141136@gmail.com>
2025-08-08 10:36:51 +0530

    exporing get,post, delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,37 @@
-const express= require("express");
+const express = require("express");
+const app = express();
 
-const app=express();
 
-app.use("/mahakal",(req,res)=>{
+//This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname: "Sumit", lastname:"Mishra"});
+});
+
+app.post("/user",(req,res)=>{
+    console.log(req.body);
+    //saving data to db
+    res.send("Data successfully saved to the databases!");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Data successfully saved to the databases!");
+});
+
+
+
+app.use("/hello", (req, res) => {
     res.send("Mahadev");
 });
 
-app.use("/jay",(req,res)=>{
-    res.send("ek bar bo de bhai bol de na bhai ");
+app.use("/jay", (req, res) => {
+    res.send("ek bar bol de bhai, bol de na bhai");
 });
 
 
-app.listen(3000, ()=>{
-    console.log("bol sankar bhagawan ki jay");
-    
+// app.use("/", (req, res) => {
+//     res.send("Sumit Mishra");
+// });
+
+app.listen(8080, () => {
+    console.log("Bol Sankar Bhagwan ki Jay");
 });
